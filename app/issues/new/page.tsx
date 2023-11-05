a6df04cbbe69d865cb23055d3089662c704676ac
sleid1 <68285202+sleid1@users.noreply.github.com>
2023-11-05 19:45:40 +0100

    Refactor: moved the inline function to onSubmit

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -28,6 +28,17 @@ const NewIssuePage = () => {
    const [error, setError] = useState('');
    const [isSubmitting, setSubmitting] = useState(false);
 
+   const onSubmit = handleSubmit(async (data) => {
+      try {
+         setSubmitting(true);
+         await axios.post('/api/issues', data);
+         router.push('/issues');
+      } catch (error) {
+         setSubmitting(false);
+         setError('An unexpected error occurred.');
+      }
+   });
+
    return (
       <div className="max-w-xl">
          {error && (
@@ -40,16 +51,7 @@ const NewIssuePage = () => {
          )}
          <form
             className="max-w-xl space-y-3"
-            onSubmit={handleSubmit(async (data) => {
-               try {
-                  setSubmitting(true);
-                  await axios.post('/api/issues', data);
-                  router.push('/issues');
-               } catch (error) {
-                  setSubmitting(false);
-                  setError('An unexpected error occurred.');
-               }
-            })}
+            onSubmit={onSubmit}
          >
             <TextField.Root>
                <TextField.Input
