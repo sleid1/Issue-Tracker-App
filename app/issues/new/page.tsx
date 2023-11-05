a1989de33031da4584cf90da089db33b600072d9
sleid1 <68285202+sleid1@users.noreply.github.com>
2023-11-05 19:14:31 +0100

    Created the ErrorMessage component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,10 +10,12 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
+   console.log(useForm());
    const router = useRouter();
    const {
       register,
@@ -52,12 +54,7 @@ const NewIssuePage = () => {
                />
             </TextField.Root>
             {errors.title && (
-               <Text
-                  color="red"
-                  as="p"
-               >
-                  {errors.title.message}
-               </Text>
+               <ErrorMessage>{errors.title.message}</ErrorMessage>
             )}
             <Controller
                name="description"
@@ -70,12 +67,7 @@ const NewIssuePage = () => {
                )}
             />
             {errors.description && (
-               <Text
-                  color="red"
-                  as="p"
-               >
-                  {errors.description.message}
-               </Text>
+               <ErrorMessage>{errors.description.message}</ErrorMessage>
             )}
             <Button>Submit New Issue</Button>
          </form>
