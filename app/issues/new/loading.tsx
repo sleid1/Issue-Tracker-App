cff9f940537ca35f478a54bc559e873d8eab03f6
sleid1 <68285202+sleid1@users.noreply.github.com>
2023-11-12 19:28:26 +0100

    Improved loading experience

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,17 +1,3 @@
-import { Box } from '@radix-ui/themes';
-import React from 'react';
-// Changed with created component for Organized imports
-// import Skeleton from 'react-loading-skeleton';
-// import 'react-loading-skeleton/dist/skeleton.css';
-import { Skeleton } from '@/app/components';
+import IssueFormSkeleton from '../_components/IssueFormSkeleton';
 
-const LoadingNewIssuePage = () => {
-   return (
-      <Box>
-         <Skeleton />
-         <Skeleton height="20rem" />
-      </Box>
-   );
-};
-
-export default LoadingNewIssuePage;
+export default IssueFormSkeleton;
