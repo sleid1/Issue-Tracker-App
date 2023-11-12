be03491d17c3ec6ebc996d065a6cf46da9ee2daa
sleid1 <68285202+sleid1@users.noreply.github.com>
2023-11-13 00:47:06 +0100

    Change the layout of the Navbar for Auth

--- app/auth/Provider.tsx
+++ app/auth/Provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
 const AuthProvider = ({ children }: PropsWithChildren) => {
    return <SessionProvider>{children}</SessionProvider>;
