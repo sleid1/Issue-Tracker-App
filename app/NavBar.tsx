b9376f45a2a23aad76027f80540f5137b294465c
sleid1 <68285202+sleid1@users.noreply.github.com>
2023-11-13 00:36:08 +0100

    Add the login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,9 +5,12 @@ import { usePathname } from 'next/navigation';
 import React, { useState } from 'react';
 import { AiFillBug } from 'react-icons/ai';
 import classnames from 'classnames';
+import { useSession } from 'next-auth/react';
+import { Box } from '@radix-ui/themes';
 
 const NavBar = () => {
    const currentPath = usePathname();
+   const { status, data: session } = useSession();
 
    const links = [
       { label: 'Dashboard', href: '/' },
@@ -21,19 +24,28 @@ const NavBar = () => {
          </Link>
          <ul className="flex space-x-6">
             {links.map((link) => (
-               <Link
-                  key={link.href}
-                  className={classnames({
-                     'text-blue-900 font-bold': link.href === currentPath,
-                     'text-zinc-500': link.href !== currentPath,
-                     'hover:text-zinc-800 transition-[color,font]': true,
-                  })}
-                  href={link.href}
-               >
-                  {link.label}
-               </Link>
+               <li key={link.href}>
+                  <Link
+                     className={classnames({
+                        'text-blue-900 font-bold': link.href === currentPath,
+                        'text-zinc-500': link.href !== currentPath,
+                        'hover:text-zinc-800 transition-[color,font]': true,
+                     })}
+                     href={link.href}
+                  >
+                     {link.label}
+                  </Link>
+               </li>
             ))}
          </ul>
+         <Box>
+            {status === 'authenticated' && (
+               <Link href="/api/auth/signout">Logout</Link>
+            )}
+            {status === 'unauthenticated' && (
+               <Link href="/api/auth/signin">Login</Link>
+            )}
+         </Box>
       </nav>
    );
 };
